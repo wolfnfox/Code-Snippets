1d743d6cf07727c3f3dace5360fb684536dd2b53
Tom <thomasf@cleardata.co.uk>
2019-07-24 11:16:59 +0100

    Added Databases project.  Initial commit.

---
+++ C# Snippets/Databases/Databases.Tests/SQLQueryClassTests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Databases.Tests
+{
+    public class SQLQueryClassTests
+    {
+        [Fact]
+        public void Initialise_SQLQuery_Class_With_Default_Parameters()
+        {
+            SQLQuery sqlQuery = new SQLQuery(null);
+            Assert.Null(sqlQuery.sql);
+            Assert.Null(sqlQuery.TObjects);
+            Assert.Null(sqlQuery.TObjects);
+            Assert.Null(sqlQuery.commandType);
+        }
+    }
+}
---
+++ C# Snippets/Databases/Databases/Databases/SQLQuery.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Databases
+{
+    public class SQLQuery<T> where T : class
+    {
+        public string sql { get; set; }
+        public string dbTable { get; set; } = null;
+        public List<T> TObjects { get; set; } = null;
+        public CommandType? commandType { get; set; } = null;
+
+        public SQLQuery(string sqlquery)
+        {
+            sql = sqlquery;
+        }
+
+        public SQLQuery(string sqlquery, T TObjectIn, string tableName = null, CommandType? commandTypeIn = null)
+        {
+            sql = sqlquery;
+            dbTable = tableName;
+            TObjects = new List<T> { TObjectIn };
+            commandType = commandTypeIn;
+        }
+
+        public SQLQuery(string sqlquery, List<T> TObjectsIn, string tableName = null, CommandType? commandTypeIn = null)
+        {
+            sql = sqlquery;
+            TObjects = TObjectsIn;
+            commandType = commandTypeIn;
+        }
+    }
+
+    public class SQLQuery : SQLQuery<object>
+    {
+        public SQLQuery(string sqlquery) : base(sqlquery)
+        {
+
+        }
+    }
+}
