40a1a94dfd21065bf917b233f72ea889e432880b
Tom <thomasf@cleardata.co.uk>
2019-07-28 19:46:03 +0100

    Updated file and tests

--- Python Snippets/helpers/file.py
+++ Python Snippets/helpers/file.py
@@ -1,24 +1,55 @@
 import logging, os, shutil, sys
 import numpy
 
-def append_all_text(string,filename,encoding=r'utf-8',overwrite=True):
-    return
+def append_all_text(text,filename,encoding=r'utf-8',overwrite=True):
+    if not isinstance(text,str):
+        raise ValueError('Invalid argument for <text>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(text)))
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
+    if not isinstance(encoding,str):
+        raise ValueError('Invalid argument for <encoding>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(encoding)))
+    if not isinstance(overwrite,bool):
+        raise ValueError('Invalid argument for <overwrite>.\nAccepted types: '+str(bool)+'\nGot type: '+str(type(overwrite)))
+    if fileexists(filename) and not overwrite:
+        raise FileExistsError()
+    return _writefile(text,filename,'at',encoding)
 
-def copy(filename):
-    return
+def copy(fromfilename,tofilename=None,overwrite=False):
+    if not isinstance(fromfilename,str):
+        raise ValueError('Invalid argument for <fromfilename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(fromfilename)))
+    if tofilename and not isinstance(tofilename,str):
+        raise ValueError('Invalid argument for <tofilename>.\nAccepted types: '+str(None)+', '+str(str)+'\nGot type: '+str(type(tofilename)))
+    if not isinstance(overwrite,bool):
+        raise ValueError('Invalid argument for <overwrite>.\nAccepted types: '+str(bool)+'\nGot type: '+str(type(overwrite)))
+    if not fileexists(fromfilename):
+        raise FileNotFoundError()
+    if (not tofilename):
+        tofilename = fromfilename
+    if (not overwrite):
+        tofilename = _incrementfilename(tofilename)
+        shutil.copy2(fromfilename,tofilename)
+    else:
+        if fileexists(tofilename):
+            move(tofilename,tofilename,overwrite)
+        else:
+            shutil.copy2(fromfilename,tofilename)
+    logging.info('Copied file: '+str(fromfilename))
+    logging.info('Copied to: '+str(tofilename))
+    return tofilename
 
 def delete(filename):
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
     if not fileexists(filename):
         raise FileNotFoundError
     os.remove(filename)
     logging.info('Deleted file: '+str(filename))
-    return
+    return True
 
 def fileexists(filename):
     if not isinstance(filename,str):
         raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
-    else:
-        return os.path.isfile(filename)
+    return os.path.isfile(filename)
 
 def filesize(filename,units=None):
     '''Returns filesize (defaults to 'KB')\n
@@ -41,43 +72,92 @@ def filesize(filename,units=None):
     return filesize
 
 def getextension(filename):
-    return
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
+    return os.path.splitext(filename)[-1]
 
-def move(fromFile,toFile,overwrite=True):
-    return
+def move(fromfilename,tofilename,overwrite=True):
+    if not isinstance(fromfilename,str):
+        raise ValueError('Invalid argument for <fromfilename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(fromfilename)))
+    if not isinstance(tofilename,str):
+        raise ValueError('Invalid argument for <tofilename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(tofilename)))
+    if not isinstance(overwrite,bool):
+        raise ValueError('Invalid argument for <overwrite>.\nAccepted types: '+str(bool)+'\nGot type: '+str(type(overwrite)))
+    if fileexists(tofilename) and (not overwrite):
+        raise FileExistsError()
+    shutil.move(fromfilename,tofilename)
+    logging.info('Moved file: '+str(fromfilename))
+    logging.info('Moved to: '+str(tofilename))
+    return True
 
 def read_all_bytes(filename):
-    return
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
+    if not fileexists(filename):
+        raise FileNotFoundError()
+    return _readfile(filename,'rb')
 
 def read_all_text(filename,encoding=r'utf-8'):
-    return
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
+    if not isinstance(encoding,str):
+        raise ValueError('Invalid argument for <encoding>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(encoding)))
+    if not fileexists(filename):
+        raise FileNotFoundError()
+    return _readfile(filename,'rt',encoding)
 
-def write_all_bytes(byteArray,filename,overwrite=True):
-    return
+def write_all_bytes(bytesdata,filename,overwrite=True):
+    if not isinstance(bytesdata,bytes):
+        raise ValueError('Invalid argument for <bytesdata>.\nAccepted types: '+str(bytes)+'\nGot type: '+str(type(bytesdata)))
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
+    if not isinstance(overwrite,bool):
+        raise ValueError('Invalid argument for <overwrite>.\nAccepted types: '+str(bool)+'\nGot type: '+str(type(overwrite)))
+    if fileexists(filename) and not overwrite:
+        raise FileExistsError()
+    return _writefile(bytesdata,filename,'wb')
 
-def write_all_text(string,filename,encoding=r'utf-8',overwrite=True):
-    return
+def write_all_text(text,filename,encoding=r'utf-8',overwrite=True):
+    if not isinstance(text,str):
+        raise ValueError('Invalid argument for <text>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(text)))
+    if not isinstance(filename,str):
+        raise ValueError('Invalid argument for <filename>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(filename)))
+    if not isinstance(encoding,str):
+        raise ValueError('Invalid argument for <encoding>.\nAccepted types: '+str(str)+'\nGot type: '+str(type(encoding)))
+    if not isinstance(overwrite,bool):
+        raise ValueError('Invalid argument for <overwrite>.\nAccepted types: '+str(bool)+'\nGot type: '+str(type(overwrite)))
+    if fileexists(filename) and not overwrite:
+        raise FileExistsError()
+    return _writefile(text,filename,'wt',encoding)
 
 def _incrementfilename(filename):
+    '''Private function to generate incremented filename if the input <filename> already exists.\n
+       Otherwise, returns the <filename> unaltered.'''
     if not fileexists(filename):
         return filename
     else:
         i = 1
-        filename = filename+'('+str(i)+')'
+        ext = getextension(filename)
+        filename = filename.replace(ext,'('+str(i)+')'+ext)
         while fileexists(filename):
             i += 1
+            filename = filename.replace(ext,'('+str(i)+')'+ext)
     return filename
 
-def _readfile(filename,options):
-    with open(filename,options) as fopen:
-        array = fopen.read()
-    return array
-
-def _writefile(array,filename,options,encoding=None):
+def _readfile(filename,options,encoding=None):
     if encoding:
         with open(filename,options,encoding) as fopen:
-            fopen.write(array)
+            data = fopen.read()
     else:
         with open(filename,options) as fopen:
-            fopen.write(array)
-    return
+            data = fopen.read()
+    return data
+
+def _writefile(data,filename,options,encoding=None):
+    if encoding:
+        with open(filename,options,encoding) as fopen:
+            fopen.write(data)
+    else:
+        with open(filename,options) as fopen:
+            fopen.write(data)
+    return True
--- Python Snippets/tests/test_file.py
+++ Python Snippets/tests/test_file.py
@@ -1,23 +1,51 @@
-import pytest
+import os, pytest, shutil
 
 from helpers import file
 
+class Tests_copy:
+    @pytest.mark.parametrize('filename', [(None,True,9,9.9)])
+    def test_copy_nonstr_raises_ValueError(self,filename):
+        with pytest.raises(ValueError):
+            file.copy(filename)
+    
+    def test_copy_raises_FileNotFoundError(self):
+        with pytest.raises(FileNotFoundError):
+            file.copy('./tests/data/fake.docx')
+
+    @pytest.mark.parametrize('filename,newfilename,expected', [('./tests/data/word.docx',None,'./tests/data/word(1).docx'),('./tests/data/word.docx','./tests/data/word(new).docx','./tests/data/word(new).docx')])
+    def test_copy_filename_returns_newfilename(self,filename,newfilename,expected):
+        newfilename = file.copy(filename,newfilename)
+        os.remove(newfilename)
+        assert newfilename == expected
+
+    def test_copy_filename_overwrites_filename(self):
+        shutil.copy2('./tests/data/word.docx','./tests/data/word-copy.docx')
+        newfilename = file.copy('./tests/data/word-copy.docx',overwrite=True)
+        os.remove(newfilename)
+        assert newfilename == './tests/data/word-copy.docx'
+
+class Tests_delete:
+    @pytest.mark.parametrize('filename', [(None,True,9,9.9)])
+    def test_delete_nonstr_raises_ValueError(self,filename):
+        with pytest.raises(ValueError):
+            file.delete(filename)
+
+    @pytest.mark.parametrize('filename', [('./tests/data/fake.docx','./tests/data/temp')])
+    def test_delete_raises_FileNotFoundError(self,filename):
+        with pytest.raises(FileNotFoundError):
+            file.delete('./tests/data/fake.docx')
+
+    def test_delete_filename(self):
+        filename = './tests/data/temp.txt'
+        with open(filename,'wt') as fopen:
+            fopen.write('test')
+        assert file.delete(filename)
+
 class Tests_fileexists:
-    def test_fileexists_None_raises_ValueError(self):
+    @pytest.mark.parametrize('filename', [(None,True,9,9.9)])
+    def test_fileexists_nonstr_raises_ValueError(self,filename):
         with pytest.raises(ValueError):
             file.fileexists(None)
-
-    def fileexists_bool_raises_ValueError(self):
-        with pytest.raises(ValueError):
-            file.fileexists(True)
-
-    def test_fileexists_int_raises_ValueError(self):
-        with pytest.raises(ValueError):
-            file.fileexists(9)
-    
-    def test_fileexists_float_raises_ValueError(self):
-        with pytest.raises(ValueError):
-            file.fileexists(9.9)
 
     def test_fileexists_returns_False_if_directory(self):
         assert not file.fileexists('./tests')
@@ -29,26 +57,35 @@ class Tests_fileexists:
         assert not file.fileexists('./helpers/files.py')
 
 class Tests_filesize:
-    def test_filesize_None_raises_ValueError(self):
+    @pytest.mark.parametrize('filename', [(None,True,9,9.9)])
+    def test_filesize_nonstr_raises_ValueError(self,filename):
         with pytest.raises(ValueError):
-            file.filesize(None)
-
-    def test_filesize_bool_raises_ValueError(self):
-        with pytest.raises(ValueError):
-            file.filesize(True)
-
-    def test_filesize_int_raises_ValueError(self):
-        with pytest.raises(ValueError):
-            file.filesize(9)
-    
-    def test_filesize_float_raises_ValueError(self):
-        with pytest.raises(ValueError):
-            file.filesize(9.9)
+            file.filesize(filename)
 
     def test_filesize_raises_FileNotFound_if_file_doesnt_exist(self):
         with pytest.raises(FileNotFoundError):
             file.filesize('./tests/data/fake.docx')
     
-    @pytest.mark.parametrize("filename,units,expected", [('./tests/data/word.docx',None, 296), ('./tests/data/word.docx','B', 302577), ('./tests/data/word.docx','KB', 296), ('./tests/data/word.docx','MB', 0.289)])
+    @pytest.mark.parametrize('filename,units,expected', [('./tests/data/word.docx',None, 296), ('./tests/data/word.docx','B', 302577), ('./tests/data/word.docx','KB', 296), ('./tests/data/word.docx','MB', 0.289)])
     def test_filesize_returns_filesize(self,filename,units,expected):
         assert file.filesize(filename,units) == expected
+
+class Tests_getextension:
+    @pytest.mark.parametrize('filename', [(None,True,9,9.9)])
+    def test_getextension_nonstr_raises_ValueError(self,filename):
+        with pytest.raises(ValueError):
+            file.getextension(filename)
+
+    @pytest.mark.parametrize('filename,expected',[('test.pdf','.pdf'),('testfile',''),('C:\test','')])
+    def test_getextension_returns_extension(self,filename,expected):
+        assert file.getextension(filename) == expected
+
+class Tests_incrementfilename:
+    @pytest.mark.parametrize('filename', [(None,True,9,9.9)])
+    def test_getextension_nonstr_raises_ValueError(self,filename):
+        with pytest.raises(ValueError):
+            file._incrementfilename(filename)
+
+    @pytest.mark.parametrize('filename,expected', [('./tests/data/fake.docx','./tests/data/fake.docx'),('./tests/data/word.docx','./tests/data/word(1).docx')])
+    def test_incrementfilename_returns_newfilename(self,filename,expected):
+        assert file._incrementfilename(filename) == expected
