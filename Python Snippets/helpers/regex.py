6bb1347c641337e5b81e348fd3c1f3b98fa4a1a5
Tom <thomasf@cleardata.co.uk>
2019-08-14 17:32:26 +0100

    Updated regex.

--- Python Snippets/helpers/regex.py
+++ Python Snippets/helpers/regex.py
@@ -2,11 +2,19 @@ import logging
 import re
 
 # Online tool for testing regular expressions
-# https://regex101.com/
+# https://regex101.com/ or https://regexr.com/
 
 windows_drive_letter = re.compile(r'^([a-zA-Z]:)')
-legal_filename_chars = re.compile(r'[a-zA-Z0-9 \.\{\}\[\]\(\)\-\_\\\/]')
-illegal_filename_chars = re.compile(r'[^a-zA-Z0-9 \.\{\}\[\]\(\)\-\_\\\/]')
+legal_filename_chars = re.compile(r'[\w \.\{\}\[\]\(\)\-\\\/]')
+illegal_filename_chars = re.compile(r'[^\w \.\{\}\[\]\(\)\-\\\/]')
 
-uk_postcode = re.compile(r'([A-Z]|[A-Z]{2})([0-9]{1}|[0-9]{2})([A-Z]? [0-9][A-Z]{2})')
-uk_vehicle_reg = re.compile(r'([A-Z]{2} ?[0-9]{2} ?[A-Z]{3})')
+email_address = re.compile(r'\b[\w\.\'\-]{1,64}\@[\w\.\'\-]{1,184}(?:\.[a-zA-Z]{2,4})\b')
+
+short_date = re.compile(r'\b(?:\d|\d{2})[ \.\-\\\/]\d{2}[ \.\-\\\/]\d{2}(?:\d{2})?\b')
+long_date = re.compile(r'\b(?:\d|\d{2}) ?(?:(?:[sS][tT])|(?:[nN][dD])|(?:[rR][dD])|(?:[tT][hH]))? ?(?:[A-Z][a-zA-Z]{2,8}) ?\d{2}(?:\d{2})?\b')
+
+currency = re.compile(r'')
+
+uk_tel_number = re.compile(r'\b(?:00 ?44|\+ ?44|0)(?: ?\d){9,10}\b')
+uk_postcode = re.compile(r'\b(?:[A-Z]|[A-Z]{2})(?:\d|\d{2})[A-Z]? \d[A-Z]{2}\b')
+uk_vehicle_reg = re.compile(r'\b[A-Z]{2}\d{2} ?[A-Z]{3}\b')
